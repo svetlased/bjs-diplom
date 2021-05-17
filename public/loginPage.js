d89c08a89fe57f2ad26e9a525714b28ca35114c1
Svetlased <svetla-sed2@mail.ru>
2021-05-17 22:07:46 +0200

    Parametr This in loginPage was deleted

--- public/loginPage.js
+++ public/loginPage.js
@@ -3,12 +3,12 @@
 const userForm = new UserForm();
 userForm.loginFormCallback = data => {
     ApiConnector.login(data, response => {
-        response.success ? location.reload() : this.setLoginErrorMessage(response.error); 
+        response.success ? location.reload() : userForm.setLoginErrorMessage(response.error); 
         });
 };
 userForm.registerFormCallback = data => {
     ApiConnector.register(data, response => {
-        response.success ? location.reload() : this.setRegisterErrorMessage(response.error); 
+        response.success ? location.reload() : userForm.setRegisterErrorMessage(response.error); 
         });
 };
  
